6cae056e7f336b8dba3527bd0b5ba3b89773cf43
Ромашенко Ярослав <ya.romashenko@samolet.ru>
2023-05-29 13:28:53 +0300

    IaC

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.65.0"
+    }
+  }
+  required_version = ">= 0.13"
+
+}
+
+provider "yandex" {
+  zone      = "ru-central1-a"
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "swarm-network"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "subnet3"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+module "swarm_cluster" {
+  source        = "./modules/instance"
+  vpc_subnet_id = yandex_vpc_subnet.subnet.id
+  managers      = 1
+  workers       = 2
+}
